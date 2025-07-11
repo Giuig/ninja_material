f6a3e0cd1338dce5099f30feb73992fe72535aac
giuliogambino <giuliogambino@protonmail.com>
2025-07-12 00:19:35 +0200

    Attempt to add centralized main

---
+++ lib/bootstrap.dart
@@ -0,0 +1,111 @@
+// lib/bootstrap.dart
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'config/shared_config.dart';
+import 'l10n/l10n.dart';
+import 'pages/first_page.dart';
+
+Future<void> runNinjaApp(
+    {required Color defaultSeedColor,
+    required LocalizationsDelegate<dynamic> localizationDelegate,
+    required String packageName,
+    required FirstPageConfig appFirstPageConfig}) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb) {
+    MobileAds.instance.initialize();
+  }
+
+  final info = await PackageInfo.fromPlatform();
+
+  globalAppName = info.appName;
+  globalVersion = info.version;
+  globalBuildNumber = info.buildNumber;
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Color.fromARGB(0, 255, 153, 0),
+      systemNavigationBarContrastEnforced: false,
+    ),
+  );
+
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  runApp(_NinjaApp(
+    defaultSeedColor: defaultSeedColor,
+    localizationDelegate: localizationDelegate,
+    appFirstPageConfig: appFirstPageConfig,
+  ));
+}
+
+class _NinjaApp extends StatefulWidget {
+  final Color defaultSeedColor;
+  final LocalizationsDelegate<dynamic> localizationDelegate;
+  final FirstPageConfig appFirstPageConfig;
+
+  const _NinjaApp({
+    required this.defaultSeedColor,
+    required this.localizationDelegate,
+    required this.appFirstPageConfig,
+  });
+
+  @override
+  State<_NinjaApp> createState() => _NinjaAppState();
+}
+
+class _NinjaAppState extends State<_NinjaApp> {
+  @override
+  void initState() {
+    super.initState();
+    globalCurrentTheme.addListener(() => setState(() {}));
+    globalCurrentLocale.addListener(() => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultLight = ColorScheme.fromSeed(
+      seedColor: widget.defaultSeedColor,
+      brightness: Brightness.light,
+    );
+    final defaultDark = ColorScheme.fromSeed(
+      seedColor: widget.defaultSeedColor,
+      brightness: Brightness.dark,
+    );
+
+    return DynamicColorBuilder(
+      builder: (lightColorScheme, darkColorScheme) {
+        final light = ColorScheme.fromSeed(
+          seedColor: lightColorScheme?.primary ?? defaultLight.primary,
+          brightness: Brightness.light,
+        );
+
+        final dark = ColorScheme.fromSeed(
+          seedColor: darkColorScheme?.primary ?? defaultDark.primary,
+          brightness: Brightness.dark,
+        );
+
+        return MaterialApp(
+          theme: ThemeData(colorScheme: light, useMaterial3: true),
+          darkTheme: ThemeData(colorScheme: dark, useMaterial3: true),
+          themeMode: globalCurrentTheme.currentTheme(context),
+          supportedLocales: L10n.all,
+          locale: globalCurrentLocale.currentLocale(context),
+          localizationsDelegates: [
+            widget.localizationDelegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          home: FirstPage.withConfig(config: widget.appFirstPageConfig),
+        );
+      },
+    );
+  }
+}
